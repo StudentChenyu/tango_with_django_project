de1e3441266a7310a7b11164ce6de2cab97e7c6c
StudentChenyu <98562062+StudentChenyu@users.noreply.github.com>
2022-02-11 11:30:57 +0000

    Chapter 3 Completed

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,12 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
-from django.conf import settings
-from django.conf.urls.static import static
+#from django.conf import settings
+#from django.conf.urls.static import static
+
+#admin.autodiscover()
+
 
 urlpatterns = [
     path('',views.index,name='index'),
     path('rango/',include('rango.urls')),
     path('admin/', admin.site.urls),
+    #path('accounts/', include('registration.backends.simple.urls')),
 ]
-static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+#static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
