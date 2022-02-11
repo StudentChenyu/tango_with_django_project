b95c69c42e5e4fd691893a7d612300535d72ceac
StudentChenyu <98562062+StudentChenyu@users.noreply.github.com>
2022-02-11 12:31:32 +0000

    Chapter 4 Chompleted

--- rango/views.py
+++ rango/views.py
@@ -18,7 +18,8 @@ from django.http import HttpResponse
 
 def index(request):
     
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
     
     """
     request.session.set_test_cookie()
