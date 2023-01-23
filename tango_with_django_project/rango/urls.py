b45bcaf2dc04dcdf587b5e8fd2f66e900df28a92
StudentChenyu <2612988z@student.gla.ac.uk>
2023-01-23 00:24:05 +0000

    Chapter 3 completed.

---
+++ tango_with_django_project/rango/urls.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 23:56:54 2023
+
+@author: Aluneth
+"""
+
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index,name='index'),
+    path('about/', views.about, name='about'),
+    ]
